d9a2e79c31d1aa6f4659873e10a45da77c636655
Manos.M <manosgr55@hotmail.com>
2022-02-02 14:44:46 +0100

    login button action

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-
+//search button action
  searchForm = document.querySelector('.header__search-form');
-
 document.querySelector('#header__search-btn').onclick = () =>{
   searchForm.classList.toggle('active');
 }
+
+
+let loginForm = document.querySelector('.login-form-container');
+//login btn action
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+} 
