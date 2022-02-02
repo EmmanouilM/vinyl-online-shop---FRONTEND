f8e906f06d7c179636dbb81e6c45bd0e752688af
Manos.M <manosgr55@hotmail.com>
2022-02-02 14:48:50 +0100

    onscroll and onload actions

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ document.querySelector('#header__search-btn').onclick = () =>{
   searchForm.classList.toggle('active');
 }
 
-
 let loginForm = document.querySelector('.login-form-container');
 //login btn action
 document.querySelector('#login-btn').onclick = () =>{
@@ -13,3 +12,20 @@ document.querySelector('#login-btn').onclick = () =>{
 document.querySelector('#close-login-btn').onclick = () =>{
   loginForm.classList.remove('active');
 } 
+
+// bottom navbar active/inactive actions
+window.onscroll = () => {
+  searchForm.classList.remove('active');
+  if (window.scrollY > window.innerHeight / 10.2) {
+    document.querySelector(".header__bottom").classList.add("header__bottom__active");
+  } else {
+    document.querySelector(".header__bottom").classList.remove("header__bottom__active");
+  }
+};
+window.onload = () => {
+  if (window.scrollY > window.innerHeight / 10.2) {
+    document.querySelector(".header__bottom").classList.add("header__bottom__active");
+  } else {
+    document.querySelector(".header__bottom").classList.remove("header__bottom__active");
+  }
+};
